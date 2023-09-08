24e6c78530554c8eefe5cba8c22b440fa5f53217
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-09-08 14:23:23 +0900

    검색기능 구현

--- src/main/java/WordCRUD.java
+++ src/main/java/WordCRUD.java
@@ -65,6 +65,19 @@ public class WordCRUD implements ICRUD{
         return idList;
     }
 
+    public void listAll(int level) {
+        int j = 0;
+        System.out.println("---------------------------------");
+        for (int i = 0; i < list.size(); i++) {
+            int ilevel = list.get(i).getLevel();
+            if(ilevel != level) continue;
+            System.out.print((j+1)+" ");
+            System.out.println(list.get(i).toString());
+            j++;
+        }
+        System.out.println("---------------------------------");
+
+    }
 
 
     public void updateItem() {
@@ -135,5 +148,17 @@ public class WordCRUD implements ICRUD{
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void searchLevel() {
+        System.out.print("=> 원하는 레벨은? (1~3) ");
+        int level = s.nextInt();
+        listAll(level);
+    }
+
+    public void searchWord() {
+        System.out.print("=> 원하는 단어는? ");
+        String keyword = s.next();
+        listAll(keyword);
     }
 }
